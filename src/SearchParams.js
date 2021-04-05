e37b11d685fed7fdcd5747ad04ac014838b61ab5
Xuffasch <tristan.lucas@gmail.com>
2021-04-05 18:59:01 +0200

    Fetch pets and display with Results component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from 'react'; 
 import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from './Results';
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown, setBreed] = useDropdown('Breed', "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown('Breed', "All", breeds);
   const [pets, setPets] = useState([]);
 
   async function requestPets() {
-    const { animals } = await pet.animal({
+    const { animals } = await pet.animals({
       location,
-      breed, type: animal
+      breed, 
+      type: animal
     })
 
     setPets(animals || []);
@@ -47,6 +49,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   )
 }
