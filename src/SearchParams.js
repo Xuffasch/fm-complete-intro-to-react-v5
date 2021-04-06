1dc9fdd3366522cf1373f040dc6d6edfe355718f
Xuffasch <tristan.lucas@gmail.com>
2021-04-06 11:02:10 +0200

    Update the submit button color on the context

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -10,7 +10,7 @@ const SearchParams = () => {
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown('Breed', "All", breeds);
   const [pets, setPets] = useState([]);
-  const [theme] = useContext(ThemeContext);
+  const [theme, setTheme] = useContext(ThemeContext);
 
   async function requestPets() {
     const { animals } = await pet.animals({
@@ -49,6 +49,22 @@ const SearchParams = () => {
         </label>
         <AnimalDropdown />
         <BreedDropdown />
+        <label htmlFor="theme">
+          Theme 
+          <select
+            value={theme.submitColor}
+            onChange={e => setTheme({ submitColor: e.target.value })}
+            onBlur={e => setTheme({ submitColor: e.target.value })}
+          >
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark Blue</option>
+            <option value="mediumorchid">Medium Orchid</option>
+            <option value="chartreuse">Chartreuse</option>
+          </select>  
+        </label>
+
+
+
         <button style={{ backgroundColor: theme.submitColor }}>Submit</button>
       </form>
       <Results pets={pets} />
