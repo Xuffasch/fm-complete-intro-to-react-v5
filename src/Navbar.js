fef37b9ef277937f9ff8b9312b1200c31a106459
Xuffasch <tristan.lucas@gmail.com>
2021-04-06 16:08:15 +0200

    Add Emotion Animation

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from '@reach/router';
-import { css } from '@emotion/core';
+import { css, keyframes } from '@emotion/core';
+import Colors from './Colors';
 
-const NavBar = () => (
-  <header
-    css={css`
-      background-color: #333;
-      padding: 15px;
-    `}
-  >
-    <Link to="/">Adopt Me!</Link>
-    <span role="img" aria-label="logo">🐩</span>
-  </header>
-)
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const color = 'pink';
+
+const NavBar = () => {
+  const [padding, setPadding] = useState(15);
+  return (
+    <header
+      onClick={() => setPadding(padding + 15)}
+      css={css`
+        background-color: ${Colors.secondary};
+        padding: ${padding}px;
+      `}
+    >
+      <Link to="/">Adopt Me!</Link>
+      <span 
+        css={css`
+          font-size: 60px;
+          display: inline-block;
+          animation: 1s ${spin} linear infinite;
+          &:hover {
+            text-decoration: underline;
+            animation: 1s ${spin} linear infinite reverse;
+          }
+        `}
+        role="img" 
+        aria-label="logo"
+
+      >
+        🐩
+      </span>
+    </header>
+  )
+}
 
 export default NavBar;
